3f25c72d011ec3238182240cb5c13030ab4587e9
Kumar Abhinav <kabhinav.iitg@gmail.com>
2025-05-20 15:35:41 +0530

    aaa

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -1,26 +1,28 @@
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const message = 'How are you?'
-const greeting = <h2>Hello World, {message}</h2>;
+// const message = 'How are you?'
+// const greeting = <h2>Hello World, {message}</h2>;
 
-const fruits = [
-  'Apple',
-  'Banana',
-  'Guava',
-  'Orange'
-];
+// const fruits = [
+//   'Apple',
+//   'Banana',
+//   'Guava',
+//   'Orange'
+// ];
 
-const fruitsILove = <ul>{fruits.map(f => <li>{f}</li>)}</ul>;
+// const fruitsILove = <ul>{fruits.map(f => <li>{f}</li>)}</ul>;
 
-const isTaskDone = true;
-const taskOutput = <h3>Task is {isTaskDone ? 'done': 'not done'}</h3>;
+// const isTaskDone = true;
+// const taskOutput = <h3>Task is {isTaskDone ? 'done': 'not done'}</h3>;
 
-const final = <div>
-  {greeting} 
-  <hr/>
-  {fruitsILove}
-  <hr/>
-  {taskOutput}
-</div>;
+// const final = <div>
+//   {greeting} 
+//   <hr/>
+//   {fruitsILove}
+//   <hr/>
+//   {taskOutput}
+// </div>;
 
-createRoot(document.getElementById("root")).render(final);
+
+createRoot(document.getElementById("root")).render(<App />);
